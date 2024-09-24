b44cd43167c3c4b342e51f5a56b8df9cbc56e77b
alexanderfilip1 <showgammer@gmail.com>
2024-09-24 12:48:14 +0300

    Implement Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../assets/css/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoePrints } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <header className="header container">
+        <nav className="header__nav">
+          <a href="/" className="header__logo">
+            <FontAwesomeIcon icon={faShoePrints} transform="rotate-270" />{" "}
+            VintageRevival
+          </a>
+          <ul className="header__list">
+            <li className="header__list--item">
+              <a href="/" className="header__list--link">
+                Home
+              </a>
+            </li>
+            <li className="header__list--item">
+              <a href="/" className="header__list--link">
+                About
+              </a>
+            </li>
+            <li className="header__list--item">
+              <a href="/" className="header__list--link">
+                Parts
+              </a>
+            </li>
+            <li className="header__list--item">
+              <a href="/" className="header__list--link">
+                Contact
+              </a>
+            </li>
+            <li className="header__list--item">
+              <button className="btn">Shop</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
