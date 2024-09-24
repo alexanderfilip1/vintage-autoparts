fbebeeeb88e2c1e6cc4cadf4a5b7522221c99006
alexanderfilip1 <showgammer@gmail.com>
2024-09-24 15:57:04 +0300

    Implement Hero Section UI

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-
+import "../assets/css/HeroSection.css";
+import logo from "../assets/images/HJcuk0lCnYSKtxeCHIEXzR-4096x4096.webp";
 export default function HeroSection() {
-  return <div>HeroSection</div>;
+  return (
+    <>
+      <section className="heroSection container">
+        <div className="heroSection__text">
+          <h1>Vintage Car Parts</h1>
+          <p>
+            Welcome to our vintage car parts store, where you&apos;ll find a
+            wide selection of high-quality parts for your classic vehicle.
+          </p>
+          <button className="shopBtn">Shop Now</button>
+        </div>
+        <div className="image__wrapper">
+          <img src={logo} alt="Classic car" className="heroSection__image" />
+        </div>
+      </section>
+    </>
+  );
 }
