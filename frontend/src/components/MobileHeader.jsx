a7b77d88abcf9ad5a42bc37a92af2b3c9e2c6e0d
alexanderfilip1 <showgammer@gmail.com>
2024-09-24 14:53:06 +0300

    Implement mobile header UI

--- frontend/src/components/MobileHeader.jsx
+++ frontend/src/components/MobileHeader.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import links from "./Links";
 import "../assets/css/MobileHeader.css";
+
 export default function MobileHeader() {
   return (
-    <>
-      <div className="mobile__nav">
-        <ul className="mobile__nav--list">
-          {links.map((item) => {
-            const { id, text, icon, path } = item;
-            return (
-              <li className="header__list--item" key={id}>
-                <a href={path} className="header__list--link">
-                  {text}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </>
+    <div className="mobile__nav">
+      <ul className="mobile__nav--list">
+        {links.map((item) => {
+          const { id, text, icon, path } = item;
+          return (
+            <li className="mobile__nav--item" key={id}>
+              <a href={path} className="mobile__nav--link">
+                {icon && <span className="mobile__nav--icon">{icon}</span>}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
