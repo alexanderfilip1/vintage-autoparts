493bd37f9b43844c6660854986a31d0e161759ef
alexanderfilip1 <showgammer@gmail.com>
2024-09-26 14:55:16 +0300

    Implement about page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import "animate.css";
+import About from "./pages/AboutPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
