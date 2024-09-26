b8551b45c7eb3dd4f1a6973b7b40fe6abb677796
alexanderfilip1 <showgammer@gmail.com>
2024-09-26 15:21:33 +0300

    Improve footer UI

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,93 +2,94 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoePrints } from "@fortawesome/free-solid-svg-icons";
 import "../assets/css/Footer.css";
+
 export default function Footer() {
   return (
     <footer className="footer container">
       <div className="footer__container">
-        <div className="footer__quickLinks">
-          <span>Quick Links</span>
-          <ul className="footer__quickLinks--list">
-            <li className="footer__quickLinks--item">
-              <a href="/" className="footer__quickLinks--anchor">
+        <div className="footer__section">
+          <h3 className="footer__title">Quick Links</h3>
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Home
               </a>
             </li>
-            <li className="footer__quickLinks--item">
-              <a href="/" className="footer__quickLinks--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 About
               </a>
             </li>
-            <li className="footer__quickLinks--item">
-              <a href="/" className="footer__quickLinks--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Parts
               </a>
             </li>
-            <li className="footer__quickLinks--item">
-              <a href="/" className="footer__quickLinks--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Contact
               </a>
             </li>
           </ul>
         </div>
-        <div className="customer__support">
-          <span>Customer Support</span>
-          <ul className="customer__support--list">
-            <li className="customer__support--item">
-              <a href="/" className="customer__support--anchor">
+        <div className="footer__section">
+          <h3 className="footer__title">Customer Support</h3>
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 FAQ
               </a>
             </li>
-            <li className="customer__support--item">
-              <a href="/" className="customer__support--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Shipping
               </a>
             </li>
-            <li className="customer__support--item">
-              <a href="/" className="customer__support--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Returns
               </a>
             </li>
-            <li className="customer__support--item">
-              <a href="/" className="customer__support--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Contact Us
               </a>
             </li>
           </ul>
         </div>
-        <div className="company__info">
-          <span>Company Info</span>
-          <ul className="customer__support--list">
-            <li className="customer__support--item">
-              <a href="/" className="customer__support--anchor">
+        <div className="footer__section">
+          <h3 className="footer__title">Company Info</h3>
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 About Us
               </a>
             </li>
-            <li className="customer__support--item">
-              <a href="/" className="customer__support--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Our Mission
               </a>
             </li>
-            <li className="customer__support--item">
-              <a href="/" className="customer__support--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Partners
               </a>
             </li>
-            <li className="customer__support--item">
-              <a href="/" className="customer__support--anchor">
+            <li className="footer__item">
+              <a href="/" className="footer__link">
                 Contact
               </a>
             </li>
           </ul>
         </div>
-        <div className="footer__copyright">
-          <FontAwesomeIcon
-            icon={faShoePrints}
-            transform="rotate-270"
-            className="shoe__icon"
-          />{" "}
-          <span>© 2024 VintageRevival, Inc. All rights reserved.</span>
-        </div>
+      </div>
+      <div className="footer__bottom">
+        <FontAwesomeIcon
+          icon={faShoePrints}
+          className="footer__icon"
+          transform="rotate-270"
+        />
+        <span>© 2024 VintageRevival, Inc. All rights reserved.</span>
       </div>
     </footer>
   );
