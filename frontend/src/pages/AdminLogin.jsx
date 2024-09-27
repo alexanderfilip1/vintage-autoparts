e90911a6f255a01204a4160810c597fdb0eac788
alexanderfilip1 <showgammer@gmail.com>
2024-09-27 12:31:50 +0300

    Implement admin login UI

---
+++ frontend/src/pages/AdminLogin.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "../assets/css/AdminLogin.css";
+import { useNavigate } from "react-router-dom";
+
+export default function AdminLogin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [notification, setNotification] = useState("");
+  const navigate = useNavigate();
+  const sendData = async (e) => {
+    setError("");
+    e.preventDefault();
+    const data = { username, password };
+    if (username === "" || password === "") {
+      setError("Username and password is required");
+      return;
+    }
+    try {
+      const res = await fetch("http://localhost:3000/api/admin-login", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+        credentials: "include",
+      });
+      const body = await res.json();
+      if (body.status === "error") {
+        setError(body.message);
+      }
+      if (body.status === "success") {
+        setNotification(`${body.message}. Redirecting...`);
+        setTimeout(() => {
+          navigate("/admin");
+        }, 2000);
+      }
+      console.log(body);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <main className="main main-bg">
+      <section className="login__section">
+        <form className="login__form" onSubmit={sendData}>
+          <h2 className="login__title">Admin Login</h2>
+          <label htmlFor="username" className="login__label">
+            Username
+            <input
+              type="text"
+              id="username"
+              className="login__input"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label htmlFor="password" className="login__label">
+            Password
+            <input
+              type="password"
+              id="password"
+              className="login__input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          {error && <span className="login__error">{error}</span>}
+          {notification && <span className="notification">{notification}</span>}
+          <button className="login__button btn" type="submit">
+            Log In
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+}
