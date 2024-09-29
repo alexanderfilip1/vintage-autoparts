eb579e55593530a14e99f8ca3a67cea9379c79b2
alexanderfilip1 <showgammer@gmail.com>
2024-09-29 14:10:13 +0300

    Remove commet

--- frontend/src/pages/ReturnsPage.jsx
+++ frontend/src/pages/ReturnsPage.jsx
@@ -12,13 +12,16 @@ export default function Returns({ logo }) {
           <section className="returns-section">
             <h1 className="returns-section__title">Return Policy</h1>
             <p className="returns-section__text">
-              We want you to be completely satisfied with your purchase. If you need to return an item, please follow our return process outlined below.
+              We want you to be completely satisfied with your purchase. If you
+              need to return an item, please follow our return process outlined
+              below.
             </p>
             <h2 className="returns-section__subtitle">Return Process</h2>
             <p className="returns-section__text">
-              To return an item, please contact our customer service team with your order number and reason for return. We will provide you with a return authorization and shipping instructions.
+              To return an item, please contact our customer service team with
+              your order number and reason for return. We will provide you with
+              a return authorization and shipping instructions.
             </p>
-            <!-- Add more return details as needed -->
           </section>
         </main>
       </div>
